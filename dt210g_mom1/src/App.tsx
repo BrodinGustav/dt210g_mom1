e4a42c2a910f83df57b63cbec56ed4020749db41
Gustav Brodin <brodin.gustav@gmail.com>
2025-01-28 14:40:59 +0100

    Commit
    - Skapat hobby.tsx som innehåller interface för props samt komponenten Book som tar emot props, samt function Hobby som håller arrayen över böcker.
    Listan itereras sedan genom .map för utskrift i App.tsx genom import

--- dt210g_mom1/src/App.tsx
+++ dt210g_mom1/src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Title from './Title';  //Importerar child-komponenten
+import Hobby from './Hobby';
 
 function App() {
- 
+
   return (
-    //Skriver ut titeln genom prop från child-komponenten
-    <div>
-    <Title name="Min titel" />  
-    </div>
+    //Skriver ut titeln genom prop från child-komponenten samt listan över hobbys
+    <>
+      <div>
+        <Title name="Min titel" />
+      </div>
+
+    
+      <Hobby />
+    </>
   )
 }
 
--- dt210g_mom1/src/Hobby.tsx
+++ dt210g_mom1/src/Hobby.tsx
@@ -7,17 +7,43 @@ interface HobbyProps {     //Interface tillåter beskrivning av vilka props komp
 }
 
 
-
-const book = ({ name, year, read }: HobbyProps) => {
+//Komponent som tar emot props
+const Book = ({ name, year, read }: HobbyProps) => {
     return (
         <div className="book">
             <h1>{name}</h1>
             <p>{year}</p>
-            <p>{read}</p>
+            <p>{read ? "Läst" : "Ej läst"}</p>
         </div>
     );
 }
 
 
+//Komponent som itererar över lista
+function Hobby() {
+
+    const hobbyArr: HobbyProps[] = [
+        { name: "Bok", year: 1979, read: true },
+        { name: "Bok", year: 1979, read: true },
+        { name: "Bok", year: 1979, read: true }
+    ];
+
+    return (
+        <div className="hobbyCard">
+            <ul>
+                {
+                    hobbyArr.map((hobby, index) => (
+                        <li key={index}>
+                            <Book name={hobby.name} year={hobby.year} read={hobby.read} />
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
+
+
+
 //Skickar titel som prop till komponent
-export default book;
+export default Hobby;
